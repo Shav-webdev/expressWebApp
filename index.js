10277ce3be9d6d530b33883efe877b0ad9c951fe
Shavarsh <shavarsh.abrahamyan1993@gmail.com>
2020-02-01 23:34:22 +0400

    added routes /, /myrout/:param, created middlware, which sets new date in cookie and renders it on UI

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-const http = require('http');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const path = require('path');
 
-console.log(__dirname)
-console.log(__filename)
+const PORT = process.env.PORT || 3000;
+const app = express();
 
-const server = http.createServer(((req, res) => {
-    console.log(req.url);
-    res.end("<h1>Hello world!!!</h1>")
-}));
+app.use(cookieParser());
 
-server.listen(3000, () => {
-    console.log("Server has been started");
+app.use(function(req, res, next) {
+  let cookie = req.cookies.todayDate;
+  if (cookie === undefined) {
+    // no: set a new cookie
+    let dateNow = new Date();
+
+    res.cookie(
+      'todayDate',
+      dateNow
+        .toString()
+        .split(',')
+        .join(','),
+      { maxAge: 900000, httpOnly: true }
+    );
+    console.log('cookie created successfully');
+  } else {
+    // yes, cookie was already present
+    console.log('cookie exists', cookie);
+  }
+  next();
+});
+
+/**
+ * router for home page*/
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello world </h1>
+            <span>${req.cookies.todayDate}</span>`);
+});
+
+/**
+ * req.param(name [, defaultValue]) Deprecated. Use either req.params, req.body or req.query, as applicable.*/
+
+app.get('/myroute/:param', (req, res) => {
+  res.send(` <pre> req.params : ${JSON.stringify(req.params)} </pre>
+             <pre>req.query : ${JSON.stringify(req.query)}</pre>  <!--If there is no query string, it is the empty object, {}-->
+             <pre>req.headers : ${JSON.stringify(req.headers)}</pre>
+             <pre> req.cookies : ${JSON.stringify(req.cookies)} </pre>`);
+  // res.send(req.query);
+});
+
+app.get('/form', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'form.html'));
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running...');
 });
